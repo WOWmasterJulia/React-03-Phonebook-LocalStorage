2745d973cfe160154c9448f52200103c778c065f
Julia <96340846+WOWmasterJulia@users.noreply.github.com>
2023-06-08 12:32:05 +0300

    Add files via upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from "./ContactForm/ContactForm";
+import { ContactList } from './ContactList/ContactList';
+import { ContactFind } from './ContactFind/ContactFind';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter:'',
+  }
+// Запись контактов с проверкой на повторы 
+  saveContact = item => {
+    if (this.state.contacts.find(elem => elem.name === item.name)) {
+      alert(`${item.name} is already in contacts.`)
+    } else {
+      this.state.contacts.push(item)
+      this.setState({ contacts: this.state.contacts })
+    }
+  };
+// Фильтр поиска кконтакта по имени
+  findChange = evt => {
+    this.setState({ filter: evt.currentTarget.value })
+  };
+// Удаление контакта из списка
+  deleteItem = id => {
+    const idContact = this.state.contacts.findIndex(contact => contact.id === id);
+    this.setState({ contact: this.state.contacts.splice(idContact, 1) })
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter(elem => elem.name.toLowerCase().includes(filter.toLowerCase()));
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm getContact={this.saveContact} />
+        <h2>Contacts</h2>
+        <ContactFind find={filter} findChange={this.findChange} />
+        <ContactList contacts={filteredContacts} deleteItem={this.deleteItem} />
+      </div>
+    )
+  }
+}
+
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
